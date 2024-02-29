f2b3afb5ef29272bcdb25a708b4014276a32824a
Вадим Акимов <sfc.coltrane@gmail.com>
2024-02-29 15:59:58 +0300

    Fixed static

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('speedtest/', include('speedtest.urls')),
 ]
-
-if settings.DEBUG:
-    # urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
-    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
